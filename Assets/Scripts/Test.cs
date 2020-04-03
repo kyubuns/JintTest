de1d138c4cca990c0b7f94f406f07c0b2e656b59
kyubuns <kyubuns@gmail.com>
2020-04-04 00:55:07 +0900

    feat: TestScript

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private Text logText = default;
+
+    public void Start()
+    {
+        logText.text = "Init...";
+        Log("Test.Start");
+        Log("Test.Finish");
+    }
+
+    private void Log(string text)
+    {
+        logText.text = $"{text}\n{logText.text}";
+    }
+}
