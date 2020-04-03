6dd767274f84aa951bed7233cedbcce76af60e58
kyubuns <kyubuns@gmail.com>
2020-04-04 08:06:16 +0900

    feat: test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Esprima.Ast;
 using Jint;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,8 +14,7 @@ public class Test : MonoBehaviour
         Log("Test.Start");
 
         var engine = new Engine()
-                .SetValue("log", new Action<object>(x => Log(x.ToString())))
-            ;
+            .SetValue("log", new Action<object>(x => Log(x.ToString())));
 
         engine.Execute(@"
             function hello() { 
@@ -24,7 +24,43 @@ public class Test : MonoBehaviour
             hello();
         ");
 
+        var add = new Engine()
+            .Execute("function add(a, b) { return a + b; }")
+            .GetValue("add");
+
+        Log($"1 + 2 = {add.Invoke(1, 2)}");
+        Log($"5 + 7 = {add.Invoke(5, 7)}");
+
+        var nanka = new Nanka
+        {
+            MonsterA = new Monster { HP = 10, Attack = 3 },
+            MonsterB = new Monster { HP = 20, Attack = 5 },
+        };
+
+        new Engine()
+            .SetValue("log", new Action<object>(x => Log(x.ToString())))
+            .SetValue("nanka", nanka)
+            .Execute(@"
+                log(`nanka.MonsterA.HP = ${nanka.MonsterA.HP}`);
+                log(`nanka.MonsterA.HP -= 5`);
+                nanka.MonsterA.HP -= 5;
+            ");
+
+        Log(nanka.MonsterA.HP.ToString());
+
         Log("Test.Finish");
+    }
+
+    public class Nanka
+    {
+        public Monster MonsterA { get; set; }
+        public Monster MonsterB { get; set; }
+    }
+
+    public class Monster
+    {
+        public int HP { get; set; }
+        public int Attack { get; set; }
     }
 
     private void Log(string text)
